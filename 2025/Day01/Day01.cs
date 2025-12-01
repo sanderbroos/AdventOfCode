e059d784388736f7e9223420af9ec32f81a5fb52
Sander Broos <sander.broos.0@gmail.com>
2025-12-01 08:24:28 +0100

    Day 1

--- 2025/Day01/Day01.cs
+++ 2025/Day01/Day01.cs
@@ -4,11 +4,47 @@ public class Day01
 {
     public static long Part1()
     {
-        return 0;
+        var directions = GetDirections();
+        var dial = 50;
+        var zeroCount = 0;
+
+        foreach (var direction in directions)
+        {
+            var dir = direction.Item1 == 'L' ? -1 : 1;
+
+            dial = (dial + direction.Item2 * dir) % 100;
+            if (dial == 0) zeroCount++;
+        }
+
+        return zeroCount;
     }
 
     public static long Part2()
     {
-        return 0;
+        var directions = GetDirections();
+        var dial = 50;
+        var zeroCount = 0;
+
+        foreach (var direction in directions)
+        {
+            var dir = direction.Item1 == 'L' ? -1 : 1;
+
+            for (int i = 0; i < direction.Item2; i++)
+            {
+                dial = (dial + dir) % 100;
+                if (dial == 0) zeroCount++;
+            }
+        }
+
+        return zeroCount;
+    }
+
+    private static List<(char, int)> GetDirections()
+    {
+        var directions = File.ReadAllLines("2025/Day01/input.txt")
+            .Select(l => (l[0], int.Parse(l[1..])))
+            .ToList();
+
+        return directions;
     }
 }
