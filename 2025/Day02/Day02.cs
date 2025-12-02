89c58cc892483593d400246e1463b748fe686483
Sander Broos <sander.broos.0@gmail.com>
2025-12-02 22:54:45 +0100

    Day 2 Part 2

--- 2025/Day02/Day02.cs
+++ 2025/Day02/Day02.cs
@@ -25,7 +25,37 @@ public class Day02
 
     public static long Part2()
     {
-        return 0;
+        var ranges = GetIDs();
+        long sumOfInvalidIds = 0;
+
+        foreach (var range in ranges)
+        {
+            for (var id = range.First(); id < range.Last() + 1; id++)
+            {
+                var idAsString = id.ToString();
+                var length = idAsString.Length;
+
+                for (var repititions = 2; repititions <= length; repititions++)
+                {
+                    if (length % repititions == 0)
+                    {
+                        List<string> partitions = [];
+                        for (var i = 0; i < length; i += length / repititions)
+                        {
+                            partitions.Add(idAsString[i..(i + length / repititions)]);
+                        }
+
+                        if (partitions.ToHashSet().Count == 1)
+                        {
+                            sumOfInvalidIds += id;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return sumOfInvalidIds;
     }
 
     private static List<List<long>> GetIDs()
